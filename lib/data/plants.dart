7f98b21c5f66f8fe54f97efb3bc24b63724f87b7
AIDIL 'AQIF <92839227+aidilaqif@users.noreply.github.com>
2024-03-22 17:47:43 +0800

    Update plants.dart

--- lib/data/plants.dart
+++ lib/data/plants.dart
@@ -7,6 +7,8 @@ class Plants {
   final String currentpH;
   final String currentWeather;
   final String currentAirMoisture;
+  final String inspections1;
+  final String inspections2;
 
   Plants({
     required this.tagging,
@@ -16,19 +18,35 @@ class Plants {
     required this.currentHumidity,
     required this.currentpH,
     required this.currentWeather,
-    required this.currentAirMoisture
+    required this.currentAirMoisture,
+    required this.inspections1,
+    required this.inspections2
   });
 
   static List<Plants> plants = [
     Plants(
-      tagging: '1',
-      plantName: 'Cili',
+      tagging: 'A1',
+      plantName: 'Cili Padi Centil',
       plantImage: 'assets/images/cili.jpeg',
       daysToHarvest: '28',
       currentHumidity: '30',
       currentpH: '7',
       currentWeather: 'Rainy',
       currentAirMoisture: '58',
+      inspections1: "pH drops from 7 to 2",
+      inspections2: "Humidity decrease to 50%"
+    ),
+    Plants(
+      tagging: 'A2',
+      plantName: 'Cili Padi Bara',
+      plantImage: 'assets/images/cili2.jpeg',
+      daysToHarvest: '28',
+      currentHumidity: '30',
+      currentpH: '7',
+      currentWeather: 'Rainy',
+      currentAirMoisture: '58',
+      inspections1: "pH drops from 7 to 2",
+      inspections2: "Humidity exceeds 90% from normal level"
     ),
     // Add more items as needed
   ];
