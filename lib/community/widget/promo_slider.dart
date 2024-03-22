d8eadfb13e6df8d1ff64771d77cd0a1cc926c3c2
FikriZaid <88956418+FikriZaid@users.noreply.github.com>
2024-03-22 16:28:29 +0800

    add sharing session

---
+++ lib/community/widget/promo_slider.dart
@@ -0,0 +1,50 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:growhub/community/home_controller.dart';
+import 'package:growhub/constants/colors.dart';
+import 'package:growhub/constants/sizes.dart';
+import 'package:growhub/containers/circular_container.dart';
+import 'package:growhub/containers/image_container.dart';
+
+
+class TPromoSlider extends StatelessWidget {
+  const TPromoSlider({
+    super.key,
+    required this.banners,
+  });
+  final List<String> banners;
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(HomeController());
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+              viewportFraction: 1,
+              onPageChanged: (index, _) => controller.updatePageIndicator(index)),
+          items: banners.map((url) => TRoundedImage(imageUrl: url)).toList(),
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems),
+        Center(
+          child: Obx(
+            () => Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                for (int i = 0; i < banners.length; i++)
+                  TCircularContainer(
+                    width: 20,
+                    height: 4,
+                    margin: const EdgeInsets.only(right: 10),
+                    backgroundColor: controller.carousalCurrentIndex.value == i
+                        ? TColors.primary
+                        : TColors.grey,
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/constants/image_strings.dart
@@ -0,0 +1,13 @@
+
+/* -- App Image Strings -- */
+
+/// This class contains all the App Images in String formats.
+class TImages {
+
+  //Banners
+  static const String promo1 = "assets/images/poster1.png";
+  static const String promo2 = "assets/images/poster2.png";
+  static const String promo3 = "assets/images/poster3.png";
+
+}
+
