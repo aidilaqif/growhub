18a05a394d15b350e5615b849c0c687ae3dc72e9
AIDIL 'AQIF MOHD PIDAUS <92839227+aidilaqif@users.noreply.github.com>
2024-03-24 18:03:11 +0800

    Update status_page.dart

--- lib/status_page.dart
+++ lib/status_page.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:growhub/pallete.dart';
 import 'package:growhub/status_items_widget.dart';
 
 import 'data/plants.dart';
 
 class StatusPage extends StatelessWidget {
   final Plants plants;
-  const StatusPage({super.key, required this.plants});
+  const StatusPage({
+    super.key,
+    required this.plants
+  });
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 50,
-          backgroundColor: Pallete.greenBasic,
-          leading: Builder(builder: (BuildContext context) {
+      appBar: AppBar(
+        toolbarHeight: 50,
+        backgroundColor: Pallete.greenBasic,
+        leading: Builder(
+          builder: (BuildContext context){
             return IconButton(
-              onPressed: () {
+              onPressed: (){
                 Navigator.pop(context);
               },
               icon: const Icon(
@@ -23,35 +29,36 @@ class StatusPage extends StatelessWidget {
                 color: Pallete.greenTertiary,
               ),
             );
-          }),
-          title: const Text(
-            "Status",
-            style: TextStyle(
-              fontSize: 30,
-              color: Pallete.greenTertiary,
-              fontWeight: FontWeight.bold,
-            ),
+          }
+        ),
+        title: const Text(
+          "Status",
+          style: TextStyle(
+            fontSize: 30,
+            color: Pallete.greenTertiary,
+            fontWeight: FontWeight.bold,
           ),
         ),
-        body: Column(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Center(
-                child: ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  itemCount: plants.statusItems.length,
-                  itemBuilder: (context, index) {
-                    final statusItem = plants.statusItems[index];
-                    return StatusItemsWidget(statusItems: statusItem);
-                  },
-                ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: plants.statusItems.length,
+                itemBuilder: (context, index){
+                  final statusItem = plants.statusItems[index];
+                  return StatusItemsWidget(statusItems: statusItem);
+                },
               ),
             ),
-            const SizedBox(
-              height: 30,
-            )
-          ],
-        ));
+          ),
+          const SizedBox(height: 30,)
+        ],
+      )
+    );
   }
 }
+
