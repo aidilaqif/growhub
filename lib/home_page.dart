b225794e62045df1aba21f86922d0a67a07ff8cc
AIDIL 'AQIF <92839227+aidilaqif@users.noreply.github.com>
2024-03-24 17:13:24 +0800

    commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:growhub/dashboard_page.dart';
 import 'package:growhub/data/plants.dart';
@@ -18,58 +17,60 @@ class _HomePageState extends State<HomePage> {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 60,
-          backgroundColor: Colors.white54,
-          title: const Text(
-            "GrowHub",
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.black,
-              fontWeight: FontWeight.bold
+          appBar: AppBar(
+            toolbarHeight: 60,
+            backgroundColor: Colors.white54,
+            title: const Text(
+              "GrowHub",
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
             ),
-          ),
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: 'All',),
-              Tab(text: 'Inspection',)
-            ]
-            ),
-        ),
-        body:  Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: TabBarView(children: [
-            GridView.builder(
-              // scrollDirection: Axis.vertical,
-              addSemanticIndexes: false,
-              primary: false,
-              padding: const EdgeInsets.all(10),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-                childAspectRatio: MediaQuery.of(context).size.width /
-              (MediaQuery.of(context).size.height / 1.8),
+            bottom: const TabBar(tabs: [
+              Tab(
+                text: 'All',
               ),
-              itemCount: Plants.plants.length,
-              itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  onTap: (){
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => DashBoardPage(plants: Plants.plants[index], index: index)));
-                  },
-                  child: PlantsCardWidget(plants: Plants.plants[index]));
-              }
-            ),
-            ListView.separated(
-              separatorBuilder: (BuildContext context, int index) => const Divider(),
-              itemCount: Plants.plants.length,
-              itemBuilder: (BuildContext context, int index) {
-                return InspectionCardsWidget(plants: Plants.plants[index]);
-              },
-            ),
-          ]),
-        )
-      ),
+              Tab(
+                text: 'Inspection',
+              )
+            ]),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: TabBarView(children: [
+              GridView.builder(
+                  // scrollDirection: Axis.vertical,
+                  addSemanticIndexes: false,
+                  primary: false,
+                  padding: const EdgeInsets.all(10),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                    childAspectRatio: MediaQuery.of(context).size.width /
+                        (MediaQuery.of(context).size.height / 1.8),
+                  ),
+                  itemCount: Plants.plants.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => DashBoardPage(
+                                  plants: Plants.plants[index], index: index)));
+                        },
+                        child: PlantsCardWidget(plants: Plants.plants[index]));
+                  }),
+              ListView.separated(
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
+                itemCount: Plants.plants.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return InspectionCardsWidget(plants: Plants.plants[index]);
+                },
+              ),
+            ]),
+          )),
     );
   }
 }
